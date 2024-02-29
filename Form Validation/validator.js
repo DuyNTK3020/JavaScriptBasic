dd0b30e587f18ef602bbb020de45e8dd75ba9b7f
Khánh Duy <duyntk3020@gmail.com>
2024-02-29 22:04:25 +0700

    update

---
+++ Form Validation/validator.js
@@ -0,0 +1,11 @@
+function Validator(options) {
+    console.log(options);
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        console.log(formElement);
+    }
+}
+
+Validator.isRequired = function () {};
+
+Validator.isEmail = function () {};
